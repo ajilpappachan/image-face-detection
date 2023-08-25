b33baaee91f87c8e75e68abda2b9e39aa601b450
Ajil Pappachan <ajilpappachan@gmail.com>
2023-08-25 23:30:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import SignIn from "./pages/SignIn";
 import Register from "./pages/Register";
 import FaceDetection from "./pages/FaceDetection";
 import Navbar from "./components/Navbar";
+import { User } from "firebase/auth";
 
 export type Page = "signin" | "register" | "facedetection";
 
 const App = () => {
 	const [currentPage, setCurrentPage] = useState<Page>("signin");
-	const [user, setUser] = useState<string>();
+	const [user, setUser] = useState<User>();
 
-	const signInUser = (newUser: string) => {
+	const signInUser = (newUser: User) => {
 		setUser(newUser);
 		setCurrentPage("facedetection");
 	};
