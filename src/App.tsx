e4d1306579ab1f786a2964ec302ace30fce09c5d
Ajil Pappachan <ajilpappachan@gmail.com>
2023-08-25 21:50:13 +0530

    Created pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import SignIn from "./pages/SignIn";
+import Register from "./pages/Register";
+import FaceDetection from "./pages/FaceDetection";
+
+type Page = "signin" | "register" | "facedetection";
+
 const App = () => {
-	return <div>App</div>;
+	const [currentPage, setCurrentPage] = useState<Page>("signin");
+
+	const changePage = (newPage: Page) => {
+		setCurrentPage(newPage);
+	};
+
+	return (
+		<div>
+			{currentPage === "signin" && <SignIn />}
+			{currentPage === "register" && <Register />}
+			{currentPage === "facedetection" && <FaceDetection />}
+		</div>
+	);
 };
 
 export default App;
---
+++ src/pages/FaceDetection.tsx
@@ -0,0 +1,5 @@
+const FaceDetection = () => {
+	return <div>FaceDetection</div>;
+};
+
+export default FaceDetection;
